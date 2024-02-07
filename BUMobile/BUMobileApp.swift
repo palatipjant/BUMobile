4399d1cd5ead7c997245e6b2db75c34a9693d86b
Palatip Jantawong <palatipjant@outlook.com>
2024-02-07 12:57:33 +0700

    Initial Commit

---
+++ BUMobile/BUMobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  BUMobileApp.swift
+//  BUMobile
+//
+//  Created by Palatip Jantawong on 7/2/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct BUMobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
